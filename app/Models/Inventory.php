ad40146ceb4af8fc6b51dfbbce4142a1d94ce03b
41119345-cloud <41119345@mahasiswa.undira.ac.id>
2026-05-16 11:48:42 +0700

    Update Inventory.php

--- app/Models/Inventory.php
+++ app/Models/Inventory.php
@@ -1,1 +1,15 @@
+<?php
 
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Inventory extends Model
+{
+    protected $fillable = ['item_name', 'category', 'status'];
+
+    public function borrowings()
+    {
+        return $this->hasMany(Borrowing::class);
+    }
+}
